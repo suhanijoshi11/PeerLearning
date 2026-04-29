44b71988ec5a428504b40d4dcb6ae04c1a324d0f
suhanijoshi11 <suhanijoshi1105@gmail.com>
2026-04-29 23:14:43 +0530

    Remove unused import of config module

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from werkzeug.utils import secure_filename
 import re
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import config
+
 
 
 app = Flask(__name__)
