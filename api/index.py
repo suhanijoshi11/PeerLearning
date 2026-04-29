262e2dea13ad64480763f752df724cd62b80e1ce
suhanijoshi11 <suhanijoshi1105@gmail.com>
2026-04-29 22:50:24 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from app import app
 
-# Vercel WSGI entry point
-app = app
+# REQUIRED for Vercel WSGI compatibility
+def handler(environ, start_response):
+    return app.wsgi_app(environ, start_response)
